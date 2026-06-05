8df19d7376534ad8924f63cc3823cbbda3668d7a
Jaya AtmakuriAIOPS <jayaatmakuriaiops@gmail.com>
2026-06-05 15:32:23 +0530

    adding infra

--- providers.tf
+++ providers.tf
@@ -27,17 +27,17 @@ provider "aws" {
 
 # Kubernetes provider will be configured after EKS cluster is created
 # Commented out to avoid errors during initial deployment
-# provider "kubernetes" {
-#   alias                  = "eks"
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     command     = "aws"
-#     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
-#   }
-# }
+provider "kubernetes" {
+  alias                  = "eks"
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
+  }
+}
 
 ###############################################################
 # Helm Provider Configuration
@@ -46,25 +46,25 @@ provider "aws" {
 provider "helm" {
 }
 
-###############################################################
+##############################################################
 # Kubectl Provider Configuration
-###############################################################
+##############################################################
 
 # Kubectl provider will be configured after EKS cluster is created
 # Commented out to avoid errors during initial deployment
-# provider "kubectl" {
-#   apply_retry_count      = 10
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   load_config_file       = false
-#
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     command     = "aws"
-#     args = [
-#       "eks", "get-token",
-#       "--cluster-name", module.eks.cluster_name,
-#       "--region", var.region
-#     ]
-#   }
-# }
+provider "kubectl" {
+  apply_retry_count      = 10
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks", "get-token",
+      "--cluster-name", module.eks.cluster_name,
+      "--region", var.region
+    ]
+  }
+}
