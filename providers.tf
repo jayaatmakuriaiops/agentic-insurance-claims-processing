a63d338e94a5c6b446f2e19bc98eac855ddd0b09
Jaya AtmakuriAIOPS <jayaatmakuriaiops@gmail.com>
2026-06-05 16:53:09 +0530

    updated helm

--- providers.tf
+++ providers.tf
@@ -44,9 +44,17 @@ provider "kubernetes" {
 ###############################################################
 
 provider "helm" {
-  kubernetes = provider.kubernetes.eks
-}
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
+    }
+  }
+}
 ##############################################################
 # Kubectl Provider Configuration
 ##############################################################
