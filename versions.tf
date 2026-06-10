882b793351388a0886c30b067e69a9a88bd4fa71
Jaya AtmakuriAIOPS <jayaatmakuriaiops@gmail.com>
2026-06-10 11:28:48 +0530

    removing infra

--- versions.tf
+++ versions.tf
@@ -10,18 +10,6 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.95.0, < 6.0.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.16, < 3.0"
-    }
-    kubectl = {
-      source  = "alekc/kubectl"
-      version = ">= 2.1"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.35, < 3.0"
-    }
     random = {
       source  = "hashicorp/random"
       version = ">= 3.6"
